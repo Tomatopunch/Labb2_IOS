717e190c9f1872968f65e26dfa6a1821768441d7
Dennis Andersson <ande1600@mace2432-03.jth-stud2.hj.se>
2019-10-30 10:46:23 +0100

    init commit

---
+++ Labb2_IOS/Labb2_IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Labb2_IOS
+//
+//  Created by Dennis Andersson on 2019-10-30.
+//  Copyright © 2019 Dennis Andersson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
